77671b92c175c7a3d2f7c7661b5ff14f91219097
kirill kozlov <kirillkozlov692@gmail.com>
2022-09-08 11:12:26 +0300

    my changes

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def send_start(message):
 weath = 0
 
 data = {1: 'января', 2: 'февраля', 3: 'марта', 4: 'апреля', 5: 'мая', 6: 'июня', 7: 'июля', 8: 'августа', 9: 'сентября', 10: 'октября', 11: 'ноября', 12: 'декабря'}
-week = {1: 'понедельник', 2: 'вторник', 3: 'среда', 4: 'четверг', 5: 'пятница', 6: 'суббота', 7: 'воскресенье'}
+week = {0: 'понедельник', 1: 'вторник', 2: 'среда', 3: 'четверг', 4: 'пятница', 5: 'суббота', 6: 'воскресенье'}
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
@@ -34,7 +34,7 @@ def send_text(message):
     elif message.text.lower() == 'пока':
         bot.send_message(message.chat.id, 'до встречи!')
     elif any([message.text.lower() == 'дата', message.text.lower() == 'время', message.text.lower() == 'date', message.text.lower() == 'time']):
-        bot.send_message(message.chat.id, f'сегодня {week[now.weekday()+1]} {now.day} {data[now.month]} {now.year} года')
+        bot.send_message(message.chat.id, f'сегодня {week[now.weekday()]} {now.day} {data[now.month]} {now.year} года')
         if now.minute >= 10:
             bot.send_message(message.chat.id, f'{now.hour}:{now.minute}')
         else:
